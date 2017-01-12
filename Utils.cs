ef18b887d4152bc17fce7c9b956309b6451efa8f
solidDoWant <http://steamcommunity.com/id/TheiInTEAM/>
2017-01-12 15:48:09 -0600

    Added a method to load PNGs

--- Utils.cs
+++ Utils.cs
@@ -1,8 +1,24 @@
 ﻿using Planetbase;
+using System.IO;
+using UnityEngine;
 
 namespace PlanetbaseFramework
 {
     class Utils
     {
+        public static Texture2D LoadPNG(string filePath)
+        {
+
+            Texture2D tex = null;
+            byte[] fileData;
+
+            if (File.Exists(filePath))
+            {
+                fileData = File.ReadAllBytes(filePath);
+                tex = new Texture2D(2, 2);
+                tex.LoadImage(fileData); //..this will auto-resize the texture dimensions.
+            }
+            return tex;
+        }
     }
 }
