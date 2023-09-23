bdfb41ac1468fbabda5d42eae6d29f4ca5a56441
solidDoWant <fred.heinecke@yahoo.com>
2023-09-22 21:26:14 -0500

    Fixed crashing issues with new ModuleTypes with custom models

--- BaseModuleType.cs
+++ BaseModuleType.cs
@@ -1,4 +1,5 @@
-﻿using Planetbase;
+﻿using System.Linq;
+using Planetbase;
 using UnityEngine;
 
 namespace PlanetbaseFramework
@@ -6,12 +7,15 @@ namespace PlanetbaseFramework
     public class BaseModuleType : ModuleType
     {
         public GameObject[] ModuleObjects { get; protected set; }
+        // This contains a copy of the GameObject once it has been loaded the first time. This prevents issues where the original GameObject has been reused or deleted in multiple places.
+        public GameObject[] ModuleObjectCache { get; protected set; }
         public string ModuleName { get; protected set; }
 
         public BaseModuleType(Texture2D icon, GameObject[] moduleObjects)
         {
             //These settings are designed to keep the game from crashing, not to provide any functionality.
-            ModuleObjects = moduleObjects;
+            ModuleObjects = moduleObjects.Select(Object.Instantiate).ToArray(); // Copy to prevent the original object from being deleted when switching out of GameStateGame
+            ModuleObjectCache = new GameObject[moduleObjects.Length];
             mIcon = icon;
             mMinSize = 0;
             mMaxSize = 0;
@@ -20,14 +24,24 @@ namespace PlanetbaseFramework
             mCost = new ResourceAmounts();
         }
 
+        // The game objects in the module cache will be null after a game is reloaded.
+        public virtual GameObject GetCachedGameObject(int index)
+        {
+            if (ModuleObjectCache[index] == null)
+                ModuleObjectCache[index] = Object.Instantiate(ModuleObjects[index]);
+
+            return ModuleObjectCache[index];
+        }
+
         public override GameObject loadPrefab(int sizeIndex)
         {
-            int adjustedSizeIndex = sizeIndex - mMinSize;   //Takes into account the edge case where mMinSize != 0
+            var adjustedSizeIndex = sizeIndex - mMinSize;   //Takes into account the edge case where mMinSize != 0
+            var moduleObject = GetCachedGameObject(adjustedSizeIndex);
 
-            ModuleObjects[adjustedSizeIndex].calculateSmoothMeshRecursive(mMeshes);
+            moduleObject.calculateSmoothMeshRecursive(mMeshes);
 
             //Add collider to object for raycasting
-            foreach (Transform transform in ModuleObjects[adjustedSizeIndex].transform)
+            foreach (Transform transform in moduleObject.transform)
             {
                 //Make the collider for the GameObject the same size/shape as the mesh
                 if (transform.gameObject.GetComponent<MeshFilter>() != null)
@@ -44,13 +58,15 @@ namespace PlanetbaseFramework
             }
 
             //This is more or less copied from PB code. Not entirely sure the effect of GroupNames on PB. TODO review this
+            // Upon transition from GameStateGame, the GroupName GameObject and all children will be destroyed.
             GameObject moduleObject2 = GameObject.Find(GroupName) ?? new GameObject { name = GroupName };
 
-            ModuleObjects[adjustedSizeIndex].transform.SetParent(moduleObject2.transform, false);
+            moduleObject.transform.SetParent(moduleObject2.transform, false);
 
-            ModuleObjects[adjustedSizeIndex].SetActive(false);
+            moduleObject.SetActive(false);
+            moduleObject.setVisibleRecursive(true);
 
-            return ModuleObjects[adjustedSizeIndex];
+            return moduleObject;
         }
     }
 }
---
+++ Patches/Planetbase/ModuleType/LoadPrefabPatch.cs
@@ -0,0 +1,28 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace PlanetbaseFramework.Patches.Planetbase.ModuleType
+{
+    // This patch is required because some change in the latest Planetbase release (1.3.8) causes
+    // `loadPrefab` overrides on instances of children of `Planetbase.ModuleType` (such as
+    // `loadPrefab` on `BaseModuleType` instances) to not properly override their parent. As a result,
+    // the new `loadPrefab` overrides will not be called, rather, their parent 
+    // `Planetbase.ModuleTYpe::loadPrefab` is called instead. This patch entirely bypasses the parent
+    // function call for ModuleTypes defined in non-Planetbase namespaces (such as
+    // `PlanetbaseFramework.BaseModuleType` and it's descendants).
+
+    [HarmonyPatch(typeof(global::Planetbase.ModuleType))]
+    [HarmonyPatch("loadPrefab")]
+    public class LoadPrefabPatch
+    {
+        public static bool Prefix(global::Planetbase.ModuleType __instance, int sizeIndex, ref GameObject __result)
+        {
+            
+            if (__instance.GetType().Namespace == typeof(global::Planetbase.ModuleType).Namespace)
+                return true;
+
+            __result = __instance.loadPrefab(sizeIndex);
+            return false;
+        }
+    }
+}
