b76661624b762201c11187629cf834e6a5c9731f
solidDoWant <fred.heinecke@yahoo.com>
2023-10-06 00:04:39 -0500

    Fixed bug where announcement download was not suppresed on certain menu pages

---
+++ Patches/Planetbase/AnnouncementManager/DownloadThreadPatch.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+
+namespace PlanetbaseFramework.Patches.Planetbase.AnnouncementManager
+{
+    [HarmonyPatch(typeof(global::Planetbase.AnnouncementManager))]
+    [HarmonyPatch("downloadThread")]
+    public class DownloadThreadPatch
+    {
+        public static bool AllowDownload { get; set; } = false;
+
+        // This patch prevents the game from "phoning home" and downloading
+        // announcements
+        public static bool Prefix()
+        {
+            return AllowDownload;
+        }
+    }
+}
