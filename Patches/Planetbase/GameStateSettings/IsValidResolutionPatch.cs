377700b1a0725201fab85751d755e43f3beef592
solidDoWant <fred.heinecke@yahoo.com>
2023-10-06 00:26:29 -0500

    Fixed ultrawide and portrait aspect ratio resolutions

---
+++ Patches/Planetbase/GameStateSettings/IsValidResolutionPatch.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+
+namespace PlanetbaseFramework.Patches.Planetbase.GameStateSettings
+{
+    [HarmonyPatch(typeof(global::Planetbase.GameStateSettings))]
+    [HarmonyPatch("isValidResolution")]
+    public class IsValidResolutionPatch
+    {
+        // This patch removes the aspect ratio check, which allows players
+        // to use ultrawide and portrait monitors without stretching.
+        public static bool Prefix(int width, int height, out bool __result)
+        {
+            __result = width >= 640 && height >= 480;
+
+            return false;
+        }
+    }
+}
