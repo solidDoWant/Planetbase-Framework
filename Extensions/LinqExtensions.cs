6d0f8af14ad366451537593dd584b0e9f7466abb
solidDoWant <fred.heinecke@yahoo.com>
2023-10-15 03:52:39 -0500

    Added random sort, random pick, and non-null selector LINQ extensions

--- Extensions/LinqExtensions.cs
+++ Extensions/LinqExtensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using PlanetbaseFramework.DataStructures;
+using Random = UnityEngine.Random;
 
 // ReSharper disable UnusedMember.Global
 
@@ -36,5 +37,42 @@ namespace PlanetbaseFramework.Extensions
         {
             return firstEnumerable.Zip(secondEnumerable).All(pair => comparisonFunction(pair.Item1, pair.Item2));
         }
+
+        /// <summary>
+        /// Randomly sorts items pseudo-randomly, using a Xorshift 128 algorithm, which results in a sort
+        /// order that is close to a uniform random distribution.
+        /// Note that this is not exactly uniform distribution as it relies on a call to OrderBy, which
+        /// uses a "stable sort". This can result in two values next to each other in the source enumerable
+        /// to remain next to each other. This has roughly a 1 in 10e14 chance of happening for any given pair.
+        /// There is a roughly a 50% chance of this occurring in every 50 trillion number pairs.
+        /// </summary>
+        public static IEnumerable<T1> OrderRandom<T1>(this IEnumerable<T1> t1Enumerable) =>
+            t1Enumerable.OrderBy(_ => Random.value);
+
+        /// <summary>
+        /// Returns a random item from the given collection, or default if there are no items in the collection.
+        /// </summary>
+        public static T1 RandomOrDefault<T1>(this ICollection<T1> t1Enumerable)
+        {
+            if (t1Enumerable.Count <= 1)
+                return t1Enumerable.FirstOrDefault();
+
+            var randomItemIndex = Random.Range(0, t1Enumerable.Count - 1);
+            using (var enumerator = t1Enumerable.GetEnumerator())
+            {
+                for (var i = 0; i < randomItemIndex; i++)
+                    if (!enumerator.MoveNext())
+                        throw new Exception(
+                            $"Attempted to get random item at {randomItemIndex}, but passed the end of the collection at position {i} first");
+
+                return enumerator.Current;
+            }
+        }
+
+        /// <summary>
+        /// Filters out null results.
+        /// </summary>
+        public static IEnumerable<T1> NonNull<T1>(this IEnumerable<T1> t1Enumerable) =>
+            t1Enumerable.Where(item => item != null);
     }
 }
